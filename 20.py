7f4092ff10f1f2b786fa4c235c0609b2d164a436
Art88789 <151448725+Art88789@users.noreply.github.com>
2026-05-21 13:56:38 +0700

    Update 20.py

--- 20.py
+++ 20.py
@@ -1,20 +1,21 @@
-def comp(a, b, m, n):
+def get_lcs_length(a, b, m, n):
+    """Находит длину наибольшей общей подпоследовательности (НОП) двух строк.
+
+    Вычисления производятся рекурсивным методом «сверху вниз» по длинам строк.
+    """
     if m == 0 or n == 0:
         return 0
 
     if a[m - 1] == b[n - 1]:
-        return 1 + comp(a, b, m - 1, n - 1)
+        return 1 + get_lcs_length(a, b, m - 1, n - 1)
 
-    res1 = comp(a, b, m - 1, n)
-    res2 = comp(a, b, m, n - 1)
-    
-    if res1 > res2:
-        return res1
-    else:
-        return res2
+    res1 = get_lcs_length(a, b, m - 1, n)
+    res2 = get_lcs_length(a, b, m, n - 1)
+
+    return max(res1, res2)
 
 
 # Пример использования
 s1 = "ABCBDAB"
 s2 = "BDCABA"
-print(comp(s1, s2, len(s1), len(s2)))  # Выведет 4
+print(get_lcs_length(s1, s2, len(s1), len(s2)))  # Выведет 4
