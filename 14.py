a3f2ecce47d2e5e0dbaf749f084a277c979f6d27
Art88789 <151448725+Art88789@users.noreply.github.com>
2026-05-21 13:53:31 +0700

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,9 +1,11 @@
-def numbers(x):
+def print_reversed_digits(x):
+    """Выводит в столбик все цифры числа 'x', начиная с конца, с помощью рекурсии.
+    """
     print(x % 10)
 
     if x > 9:
-        numbers(x // 10)
+        print_reversed_digits(x // 10)
 
 
 # Пример использования
-numbers(123)
+print_reversed_digits(123)
