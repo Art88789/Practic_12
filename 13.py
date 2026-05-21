3d08ad46f5099c944feb21392b5d4b3c46d52a63
Art88789 <151448725+Art88789@users.noreply.github.com>
2026-05-21 13:53:04 +0700

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,13 +1,15 @@
-def odd_list(a, n):
+def get_even_numbers(a, n):
+    """Выбирает все чётные числа из первых 'n' элементов списка рекурсивно.
+    """
     if n == 0:
         return []
-    
-    if a[0] % 2 == 0:
-        return [a[0]] + odd_list(a[1:], n - 1)
 
-    return odd_list(a[1:], n - 1)
+    if a[0] % 2 == 0:
+        return [a[0]] + get_even_numbers(a[1:], n - 1)
+
+    return get_even_numbers(a[1:], n - 1)
 
 
 # Пример использования
 numbers = [1, 2, 3, 4, 5, 6]
-print(odd_list(numbers, 6))  # Выведет [2, 4, 6]
+print(get_even_numbers(numbers, 6))  # Выведет [2, 4, 6]
