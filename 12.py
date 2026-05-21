26faca634142149ba5cba8b161709e95a24b068b
Art88789 <151448725+Art88789@users.noreply.github.com>
2026-05-21 13:52:15 +0700

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,14 +1,16 @@
-def search(a, x):
+def has_element(a, x):
+    """Проверяет, содержится ли элемент 'x' в списке 'a' рекурсивным способом.
+    """
     if len(a) == 0:
-        return 0
-    
+        return False
+
     if a[0] == x:
-        return 1
-    
-    return search(a[1:], x)
+        return True
+
+    return has_element(a[1:], x)
 
 
 # Пример использования
 numbers = [1, 5, 8, 12, 3]
-print(search(numbers, 8))   # Выведет 1
-print(search(numbers, 10))  # Выведет 0
+print(has_element(numbers, 8))   # Выведет True
+print(has_element(numbers, 10))  # Выведет False
