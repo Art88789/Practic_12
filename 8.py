64a1a00d0f3625da9f243a829083a80049508352
Art88789 <151448725+Art88789@users.noreply.github.com>
2026-05-21 13:50:24 +0700

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,9 +1,11 @@
-def fib(k):
+def get_fibonacci_number(k):
+    """Находит k-й член последовательности Фибоначчи рекурсивным способом.
+    """
     if k == 1 or k == 2:
         return 1
-    
-    return fib(k - 1) + fib(k - 2)
+
+    return get_fibonacci_number(k - 1) + get_fibonacci_number(k - 2)
 
 
 # Пример: 7-й член последовательности Фибоначчи (1, 1, 2, 3, 5, 8, 13)
-print(fib(7))
+print(get_fibonacci_number(7))
