05071a0cf61ac783affd0a21e978aff27e6335ec
Art88789 <151448725+Art88789@users.noreply.github.com>
2026-05-21 13:51:17 +0700

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,15 +1,17 @@
-def maxlist(a):
+def get_max_element(a):
+    """Находит максимальный элемент в списке рекурсивным способом.
+    """
     if len(a) == 1:
         return a[0]
-    
-    m = maxlist(a[1:])
-    
-    if a[0] > m:
+
+    max_of_rest = get_max_element(a[1:])
+
+    if a[0] > max_of_rest:
         return a[0]
-    
-    return m
+
+    return max_of_rest
 
 
 # Пример использования
 numbers = [3, 1, 7, 2, 5]
-print(maxlist(numbers))
+print(get_max_element(numbers))
