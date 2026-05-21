49279e0b5a2d27a31d33e96df8bbfa4f27adb8d1
Art88789 <151448725+Art88789@users.noreply.github.com>
2026-05-21 13:48:26 +0700

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,9 +1,11 @@
-def progress(a1, r, n):
+def get_arithmetic_element(a1, r, n):
+    """Находит n-й член арифметической прогрессии рекурсивным способом.
+    """
     if n == 1:
         return a1
-    
-    return progress(a1, r, n - 1) + r
+
+    return get_arithmetic_element(a1, r, n - 1) + r
 
 
 # Пример: первый член 5, разность 2, найти 10-й член
-print(progress(5, 2, 10))
+print(get_arithmetic_element(5, 2, 10))
