253e0ef5bd5fdaba3f6054db19d3d348f39cc04a
Art88789 <151448725+Art88789@users.noreply.github.com>
2026-05-21 13:54:22 +0700

    Update 16.py

--- 16.py
+++ 16.py
@@ -1,16 +1,22 @@
-def get_digit(digit):
+def get_hex_char(digit):
+    """Возвращает символьное представление цифры для систем счисления до 16.
+    """
     digits = "0123456789ABCDEF"
     return digits[digit]
 
 
-def ten_to_n(x, n):
+def convert_from_decimal(x, n):
+    """Переводит число из десятичной системы в систему счисления с основанием n.
+
+    Использует рекурсивный алгоритм деления на основание системы.
+    """
     if x < n:
-        return get_digit(x)
-    
-    return ten_to_n(x // n, n) + get_digit(x % n)
+        return get_hex_char(x)
+
+    return convert_from_decimal(x // n, n) + get_hex_char(x % n)
 
 
 # Примеры использования
-print(ten_to_n(10, 2))   # Выведет "1010"
-print(ten_to_n(255, 16)) # Выведет "FF"
-print(ten_to_n(12, 8))   # Выведет "14"
+print(convert_from_decimal(10, 2))   # Выведет "1010"
+print(convert_from_decimal(255, 16)) # Выведет "FF"
+print(convert_from_decimal(12, 8))   # Выведет "14"
